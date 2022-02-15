3ff128c15a6323f52d5f4014836362c0cb12b9bc
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-15 18:16:03 +0900

    :memo: #1.4 | WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to the Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log(`Received message: `, message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from the Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("Hello from the browser!");
+}, 10_000);
