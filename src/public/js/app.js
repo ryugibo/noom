2764d8996aabb0b14bdfea6ad481fa2d1a47f95b
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-20 12:49:38 +0900

    :memo: #3.0 | User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,43 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  muted = !muted;
+  if (muted) {
+    muteBtn.innerText = "Unmute";
+  } else {
+    muteBtn.innerText = "Mute";
+  }
+}
+
+function handleCameraClick() {
+  cameraOff = !cameraOff;
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera On";
+  } else {
+    cameraBtn.innerText = "Turn Camera Off";
+  }
+}
+mute.addEventListener("click", handleMuteClick);
+camera.addEventListener("click", handleCameraClick);
